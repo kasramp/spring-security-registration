c0303098a7e38db162571e9c948f154b5ae02241
kasramp <kasra_mp@yahoo.com>
2023-03-05 12:50:57 +0100

    Handle access denied if the role is not management or admin

--- src/main/java/com/baeldung/web/controller/ManagementController.java
+++ src/main/java/com/baeldung/web/controller/ManagementController.java
@@ -1,4 +1,16 @@
 package com.baeldung.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/management")
 public class ManagementController {
+
+    @GetMapping
+    public ModelAndView viewManagement() {
+        return new ModelAndView("private/management");
+    }
 }
